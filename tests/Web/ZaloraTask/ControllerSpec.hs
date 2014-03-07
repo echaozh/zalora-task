156d92b9d61bc63d20e221c19bb65074d41ce299
Zhang Yichao <echaozh@gmail.com>
2014-03-08 07:46:58 +0800

    cleaned up test code, now only depends on controller's public intf

--- tests/Web/ZaloraTask/ControllerSpec.hs
+++ tests/Web/ZaloraTask/ControllerSpec.hs
@@ -28,16 +28,106 @@ import Test.Hspec
 import Text.HandsomeSoup
 import Text.XML.HXT.Core hiding (app)
 
-import Web.Scotty.Trans (scottyAppT, get, post)
+import Web.Scotty.Trans (scottyAppT)
 
-import Web.ZaloraTask
+import Web.ZaloraTask (handleAppError)
 import Web.ZaloraTask.Controller hiding (Shoe)
 import Web.ZaloraTask.Model
 import Web.ZaloraTask.Types hiding (photoDir, pool)
 
-withPool :: (ConnectionPool -> IO a) -> (ConnectionPool -> IO ()) -> IO ()
-withPool bef action =
-  withSqlitePool ":memory:" 1 $ bef &&& action ⋙ arr (uncurry (>>))
+spec :: Spec
+spec =
+  before (createDirectoryIfMissing True photoDir)
+  $ after_ (removeDirectoryRecursive photoDir
+            `catch` \e -> void $ return (e::IOException))
+  $ around (withPool fixture)
+  $ makeShoesSpec >> showShoesSpec >> listShoesSpec
+
+makeShoesSpec :: SpecWith ConnectionPool
+makeShoesSpec = describe "makeShoes" $ do
+  let goodReq = SRequest (setPath defaultRequest{requestMethod="POST"} "/shoes")
+                $ BSC.pack ("{\"description\":\"just shoes\",\"color\":\"red\","
+                           ++ "\"size\":\"35\",\"photo\":\"" ++ photoEncoded
+                           ++ "\"}")
+      badReq  = SRequest (setPath defaultRequest{requestMethod="POST"} "/shoes")
+                $ ""
+
+  context "When everything is fine" $ do
+      it "redirects to showShoes" $ \pool -> do
+        resp <- run goodReq pool
+        simpleStatus resp `shouldBe` found302
+        lookup hLocation (simpleHeaders resp) `shouldBe` Just "/shoes/6"
+
+      it "saves the photo to disk base 64 decoded" $ \pool -> do
+        void $ run goodReq pool
+        readFile (photoDir <> photoPathFor photoName) `shouldReturn` photoRaw
+
+  context "When provided with invalid input" $ do
+    it "reports bad request" $ \pool ->
+      statusFor badReq pool `shouldReturn` badRequest400
+
+  context "When db fails" $ do
+    it "reports internal server error" $ \pool -> do
+      dropTable pool
+      statusFor goodReq pool `shouldReturn` internalServerError500
+
+showShoesSpec :: SpecWith ConnectionPool
+showShoesSpec = describe "showShoes" $ do
+  context "When requesting existing shoes" $ do
+    it "reports ok" $ \pool ->
+      statusFor' "/1" pool `shouldReturn` ok200
+
+    it "returns html with shoe data" $ \pool ->
+      map head <$> sequence (map (raht "/1" pool) ["#description", "#color",
+                                                   "#size"]
+                             ++ [raha "/1" pool "#photo" "src"])
+         `shouldReturn` parsedShoe
+
+  context "When requesting non-existent shoes" $ do
+    it "reports not found" $ \pool ->
+      statusFor' "/100" pool `shouldReturn` notFound404
+
+  context "When requesting with an improper id" $ do
+    it "reports not found" $ \pool ->
+      statusFor' "/NaN" pool `shouldReturn` notFound404
+
+listShoesSpec :: SpecWith ConnectionPool
+listShoesSpec = describe "listShoes" $ do
+  let pathFor = ("?p="<>) . (show::Int -> String)
+
+  context "Without paging" $ do
+    it "links to all shoes" $ \pool ->
+      extractShoeList "" pool `shouldReturn` shoeList 1 5
+
+  context "With paging" $ do
+    it "links to shoes on current page" $ \pool -> do
+      extractShoeList (pathFor 2) pool `shouldReturn` shoeList 3 4
+      extractShoeList (pathFor 3) pool `shouldReturn` shoeList 5 5
+
+    context "and page number is not right" $ do
+      it "reports not found for non-existent page" $ \pool ->
+        statusFor' (pathFor 100) pool `shouldReturn` notFound404
+
+      it "reports bad request for invalid page number" $ \pool ->
+        statusFor' "?p=NaN" pool `shouldReturn` badRequest400
+
+    context "and a previous page" $
+      it "links to that" $ \pool -> do
+        raha (pathFor 2) pool "#prev" "href" `shouldReturn` ["/shoes?p=1"]
+        raha (pathFor 3) pool "#prev" "href" `shouldReturn` ["/shoes?p=2"]
+
+    context "and a next page" $
+      it "links to next page if there is one" $ \pool -> do
+        raha (pathFor 1) pool "#next" "href" `shouldReturn` ["/shoes?p=2"]
+        raha (pathFor 2) pool "#next" "href" `shouldReturn` ["/shoes?p=3"]
+
+    context "and no previous page" $
+      it "doesn't have previous link" $ \pool ->
+      raha (pathFor 1) pool "#prev" "href" `shouldReturn` []
+
+    context "and no next page" $
+      it "doesn't have next link" $ \pool ->
+      raha (pathFor 3) pool "#next" "href" `shouldReturn` []
 
 defaultShoe :: Shoe
 defaultShoe = Shoe "default shoes" "black" 43 (pack photoName)
@@ -46,14 +136,6 @@ parsedShoe :: [Text]
 parsedShoe = map ($ defaultShoe) [shoeDescription, shoeColor,
                                   pack . show . shoeSize,
                                   photoPathFor . shoePhoto]
-
-fixture :: ConnectionPool -> IO ()
-fixture = runSqlPersistMPool $ do
-  runMigration migrate
-  replicateM_ 5 $ insert defaultShoe
-
-dropTable :: ConnectionPool -> IO ()
-dropTable = runSqlPersistMPool $ rawExecute "drop table shoe" []
 
 photoDir :: FilePath
 photoName :: String
@@ -68,53 +150,32 @@ photoEncoded :: String
 photoRaw = "1234\n"
 photoEncoded = "MTIzNAo="
 
-spec :: Spec
-spec =
-  before (createDirectoryIfMissing True photoDir)
-  $ after_ (removeDirectoryRecursive photoDir
-            `catch` \e -> void $ return (e::IOException))
-  $ around (withPool fixture)
-  $ makeShoesSpec >> showShoesSpec >> listShoesSpec
+withPool :: (ConnectionPool -> IO a) -> (ConnectionPool -> IO ()) -> IO ()
+withPool bef action =
+  withSqlitePool ":memory:" 1 $ bef &&& action ⋙ arr (uncurry (>>))
 
-run :: AppM Connection IO () -> SRequest -> ConnectionPool -> IO SResponse
-run act req p = do
-  r <- runSession (srequest req) =<< app
-  print r
-  return r
+fixture :: ConnectionPool -> IO ()
+fixture = runSqlPersistMPool $ do
+  runMigration migrate
+  replicateM_ 5 $ insert defaultShoe
+
+dropTable :: ConnectionPool -> IO ()
+dropTable = runSqlPersistMPool $ rawExecute "drop table shoe" []
+
+run :: SRequest -> ConnectionPool -> IO SResponse
+run req p = runSession (srequest req) =<< app
   where
-    app = scottyAppT (runApp config) (runApp config) $ handleAppError >> act
+    app = scottyAppT (runApp config) (runApp config) $ handleAppError >> routes
     config = AppConfig photoDir p 2
 
+statusFor :: SRequest -> ConnectionPool -> IO Status
+statusFor = (simpleStatus<$>) .: run
+
+statusFor' :: String -> ConnectionPool -> IO Status
+statusFor' path = (simpleStatus<$>) . run (reqFor path)
+
 reqFor :: String -> SRequest
-reqFor = flip SRequest "" . setPath defaultRequest . BS.pack
-
-makeShoesSpec :: SpecWith ConnectionPool
-makeShoesSpec = describe "makeShoes" $ do
-  let run'    = run $ post "/" makeShoes
-      goodReq = SRequest defaultRequest{requestMethod="POST"}
-                $ BSC.pack ("{\"description\":\"just shoes\",\"color\":\"red\","
-                           ++ "\"size\":\"35\",\"photo\":\"" ++ photoEncoded
-                           ++ "\"}")
-      badReq  = SRequest defaultRequest{requestMethod="POST"} ""
-
-  context "When everything is fine" $ do
-      it "redirects to showShoes" $ \pool ->
-        ((simpleStatus &&& (lookup hLocation . simpleHeaders))
-         <$> run' goodReq pool)
-        `shouldReturn` (found302, Just "/shoes/6")
-
-      it "saves the photo to disk base 64 decoded" $ \pool -> do
-        void $ run' goodReq pool
-        readFile (photoDir <> photoPathFor photoName) `shouldReturn` photoRaw
-
-  context "When provided with invalid input" $ do
-    it "reports bad request" $ \pool ->
-      simpleStatus <$> run' badReq pool `shouldReturn` badRequest400
-
-  context "When db fails" $ do
-    it "reports internal server error" $ \pool -> do
-      dropTable pool
-      simpleStatus <$> run' goodReq pool `shouldReturn` internalServerError500
+reqFor = flip SRequest "" . setPath defaultRequest . BS.pack . ("/shoes"<>)
 
 cssText :: String -> LA XmlTree String
 cssText = css >>> (//>getText)
@@ -122,93 +183,23 @@ cssText = css >>> (//>getText)
 cssAttr :: String -> String -> LA XmlTree String
 cssAttr sel = (css sel >>>) . getAttrValue
 
-runAndHXT :: (SRequest -> ConnectionPool -> IO SResponse)
-          -> String -> ConnectionPool -> LA XmlTree String -> IO [Text]
-runAndHXT run' req pool hxt = do
-  html' <- simpleBody <$> run' (reqFor req) pool
+runAndHXT :: String -> ConnectionPool -> LA XmlTree String -> IO [Text]
+runAndHXT req pool hxt = do
+  html' <- simpleBody <$> run (reqFor req) pool
   return . map pack $ runLA (hreadDoc >>> hxt) $ BSC.unpack html'
 
-raht :: (SRequest -> ConnectionPool -> IO SResponse)
-     -> String -> ConnectionPool -> String -> IO [Text]
-raht run' req pool = runAndHXT run' req pool .  cssText
+raht :: String -> ConnectionPool -> String -> IO [Text]
+raht req pool = runAndHXT req pool .  cssText
 
-raha :: (SRequest -> ConnectionPool -> IO SResponse)
-     -> String -> ConnectionPool -> String -> String -> IO [Text]
-raha run' req pool = runAndHXT run' req pool .: cssAttr
-
-showShoesSpec :: SpecWith ConnectionPool
-showShoesSpec = describe "showShoes" $ do
-  let run'    = run $ get "/:id" showShoes
-
-  context "When requesting existing shoes" $ do
-    it "reports ok" $ \pool ->
-      simpleStatus <$> run' (reqFor "/1") pool `shouldReturn` ok200
-    it "returns html with shoe data" $ \pool ->
-      let raht' = raht run' "/1" pool
-      in map head <$> sequence [raht' "#description", raht' "#color",
-                                raht' "#size",
-                                raha run' "/1" pool "#photo" "src"]
-         `shouldReturn` parsedShoe
-
-  context "When requesting non-existent shoes" $ do
-    it "reports not found" $ \pool ->
-      simpleStatus <$> run' (reqFor "/100") pool `shouldReturn` notFound404
-
-  context "When requesting with an improper id" $ do
-    it "reports not found" $ \pool ->
-      simpleStatus <$> run' (reqFor "/NaN") pool `shouldReturn` notFound404
+raha :: String -> ConnectionPool -> String -> String -> IO [Text]
+raha req pool = runAndHXT req pool .: cssAttr
 
 shoeList :: Int -> Int -> [[Text]]
 shoeList from to = transpose [map pack ["Shoe #" <> show n, "/shoes/" ++ show n,
                                         "/" ++ photoName ++ ".jpg"]
                              | n <- [from .. to]]
 
-extractShoeList :: (SRequest -> ConnectionPool -> IO SResponse)
-                -> String -> ConnectionPool -> IO [[Text]]
-extractShoeList run' path pool =
-  sequence [raht run' path pool ".shoe", raha' ".link" "href",
-            raha' ".photo" "src"]
-  where raha' = raha run' path pool
-
-listShoesSpec :: SpecWith ConnectionPool
-listShoesSpec = describe "listShoes" $ do
-  let pathFor = ("/?p="<>) . (show::Int -> String)
-
-  context "Without paging" $ do
-    let run' = run $ get "/" listAllShoes
-
-    it "links to all shoes" $ \pool ->
-      extractShoeList run' "/" pool `shouldReturn` shoeList 1 5
-
-  context "With paging" $ do
-    let run' = run $ get "/" listShoes
-
-    it "links to shoes on current page" $ \pool -> do
-      extractShoeList run' (pathFor 2) pool `shouldReturn` shoeList 3 4
-      extractShoeList run' (pathFor 3) pool `shouldReturn` shoeList 5 5
-
-    context "and page number is not right" $ do
-      it "reports not found for non-existent page" $ \pool ->
-        simpleStatus <$> run' (reqFor $ pathFor 100) pool
-        `shouldReturn` notFound404
-      it "reports bad request for invalid page number" $ \pool ->
-        simpleStatus <$> run' (reqFor "/?p=NaN") pool
-        `shouldReturn` badRequest400
-
-    context "and a previous page" $
-      it "links to that" $ \pool -> do
-        raha run' (pathFor 2) pool "#prev" "href" `shouldReturn` ["/shoes?p=1"]
-        raha run' (pathFor 3) pool "#prev" "href" `shouldReturn` ["/shoes?p=2"]
-
-    context "and a next page" $
-      it "links to next page if there is one" $ \pool -> do
-        raha run' (pathFor 1) pool "#next" "href" `shouldReturn` ["/shoes?p=2"]
-        raha run' (pathFor 2) pool "#next" "href" `shouldReturn` ["/shoes?p=3"]
-
-    context "and no previous page" $
-      it "doesn't have previous link" $ \pool ->
-      raha run' (pathFor 1) pool "#prev" "href" `shouldReturn` []
-
-    context "and no next page" $
-      it "doesn't have next link" $ \pool ->
-      raha run' (pathFor 3) pool "#next" "href" `shouldReturn` []
+extractShoeList :: String -> ConnectionPool -> IO [[Text]]
+extractShoeList path pool =
+  sequence (raht path pool ".shoe" : (map (uncurry $ raha path pool)
+                                      [(".link", "href"), (".photo", "src")]))
