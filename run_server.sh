94075b419f1759dcb725ad76bd1da2979a6fad16
Zhang Yichao <echaozh@gmail.com>
2014-03-09 14:52:52 +0800

    add error pages to nginx setup

--- run_server.sh
+++ run_server.sh
@@ -4,13 +4,24 @@ set -e
 
 photodir="$1"
 connstr="$2"
+test="$3"
 
 wd=$(dirname $(realpath $0))
 cd "$wd"
 
+function on_exit {
+    kill $(cat /tmp/nginx.pid)
+    rm /tmp/nginx.{error,access}.log
+    [ -n test ] && rm -rf "$photodir"
+}
+
 mkdir -p "$photodir"
-echo set '$photodir' $photodir';' > env.vars
-trap "killall nginx" EXIT
+cat > env.vars <<EOF
+set \$photodir $photodir;
+set \$errdir $wd;
+EOF
+
+trap on_exit EXIT
 nginx -c "$wd/nginx.conf" && echo Nginx started
 
 cabal run migrate "$connstr" && echo Shoe table migrated
